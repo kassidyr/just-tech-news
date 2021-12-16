c2cc24ee0ce512cb9c2eebc90f8630e57bd9e69c
kassidyr <kassidy.ralph@gmail.com>
2021-12-16 15:36:29 -0500

    created post-routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+// creates the reference for the id column in the User model to link to the corresponding foreign key pair, which is user_id in the Post model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// creates the reverse association: the reference for the id column in the Post model to link to the corresponding foreign key pair which is user_id in the User model; the constraint is that the Post belongs to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+// force: true makes the database connection sync with the model definitions and associations; the tables re-create if there are association changes (similar to DROP TABLE IF EXISTS; this allows the table to be overwritten and re-created)
+// if force: false and the table exists, any field additions/modifications/deletions you have won't be executed
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
